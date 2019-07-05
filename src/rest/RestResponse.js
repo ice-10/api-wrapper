fbf73cf96b088af8a2943acbbb6e5bee0d66274b
Oskar <oskar.risto.lang@gmail.com>
2019-07-05 17:19:21 +0200

    Documented constructors and methods

--- src/rest/RestResponse.js
+++ src/rest/RestResponse.js
@@ -1,5 +1,10 @@
 class RestResponse {
 
+  /**
+   * Creates a new RestResponse out of a given axios response
+   * 
+   * @param {object} response The response that the axios client returned
+   */
   constructor(response) {
     this.code = response.status || response.response.status;
     this.body = response.data || response.response.data;
--- src/utils/HttpUtil.js
+++ src/utils/HttpUtil.js
@@ -3,11 +3,23 @@ const RestResponse = require('../rest/RestResponse');
 
 class Http {
 
+  /**
+   * Creates a new instance of the Http Utils
+   * 
+   * @param {Client} client The Client which created this instance
+   */
   constructor(client) {
     this.client = client;
     axios.defaults.baseURL = this.client.baseURL;
   }
 
+  /**
+   * Creates a new request
+   * 
+   * @param {string} method The method to use for the request
+   * @param {string} url The URL to send the request to
+   * @param {object} param2 Optional headers and body
+   */
   request(method, url, { headers = {}, body = {} }) {
     return axios.request({
       method: method,
