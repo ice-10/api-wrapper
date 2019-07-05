cba3fcbe5bd85c476a987a2fe9b0239bf72e30be
Oskar <oskar.risto.lang@gmail.com>
2019-07-05 17:19:45 +0200

    Implemented Chargebee-related endpoints and documented code

--- src/client/Client.js
+++ src/client/Client.js
@@ -2,25 +2,79 @@ const HttpUtil = require('../utils/HttpUtil');
 
 class Client {
 
+  /**
+   * Create a new instance of an API Client
+   * 
+   * @param {object} options Basic options needed to configure the Client
+   */
   constructor(options) {
     this.baseURL = options.baseURL;
     this.http = new HttpUtil(this);
   }
 
+  /**
+   * Returns access and refresh tokens
+   *  
+   * @param {string} code The authorization code Discord returned
+   */  
   authorize(code) {
     return this.http.request('post', '/tokens', { body: { code: code }});
   }
 
+  /**
+   * Returns a new set of access and refresh tokens
+   * 
+   * @param {string} token The refresh token our API returned
+   */
   refresh(token) {
     return this.http.request('post', '/tokens', { body: { refreshToken: token }});
   }
 
+  /**
+   * Returns the user object related to the provided token
+   * 
+   * @param {string} token The access token our API returned
+   */
   getSelfUser(token) {
     return this.http.request('get', '/users/@me', { headers: { 'authorization': `Bearer ${token}` } });
   }
 
+  /**
+   * Returns all guilds the user is in related to the provided token
+   * 
+   * @param {string} token The access token our API returned
+   */
   getSelfGuilds(token) {
     return this.http.request('get', '/users/@me/guilds', { headers: { 'authorization': `Bearer ${token}` } });
+  }
+
+  /**
+   * Returns the subscription object related to the provided token
+   * 
+   * @param {string} token The access token our API returned
+   */
+  getSelfSubscription(token) {
+    return this.http.request('get', '/users/@me/subscription', { headers: { 'authorization': `Bearer ${token}` } });
+  }
+
+  /**
+   * Applies Premium to the specified guild
+   * 
+   * @param {string} token The access token our API returned
+   * @param {string} id The Discord guild id
+   */
+  applyPremium(token, id) {
+    return this.http.request('post', `/guilds/${id}/premium`, { headers: { 'authorization': `Bearer ${token}` } });
+  }
+
+  /**
+   * 
+   * 
+   * @param {string} token The access token our API returned
+   * @param {string} id The Discord guild id
+   */
+  removePremium(token, id) {
+    return this.http.request('delete', `/guilds/${id}/premium`, { headers: { 'authorization': `Bearer ${token}` } });
   }
 }
 
